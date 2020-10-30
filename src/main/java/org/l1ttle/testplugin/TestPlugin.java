da984090c9cc1bc5d1806f637fb702c4b1573805
l1ttleO <61277953+l1ttleO@users.noreply.github.com>
2020-10-30 14:15:15 +0500

    Refactor (#8)
    * Add newlines and update plugin.yml
    
    * Output friendly name of the player

--- src/main/java/org/l1ttle/testplugin/TestPlugin.java
+++ src/main/java/org/l1ttle/testplugin/TestPlugin.java
@@ -1,10 +1,17 @@
 package org.l1ttle.testplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
 
-public final class TestPlugin extends JavaPlugin {
+public final class TestPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
@@ -14,7 +21,27 @@ public final class TestPlugin extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        getLogger().log(Level.INFO, "Pong! We are disabling");
         // Plugin shutdown logic
+        getLogger().log(Level.INFO, "Pong! We are disabling");
     }
+
+    @EventHandler
+    void onReload() {
+        getLogger().severe("RELOAD DETECTED. Please avoid reloading! Reloading can cause many unexpected bugs.");
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("testperm")) {
+            if (args.length == 2) {
+                Player player = Bukkit.getPlayer(args[0]);
+                String playerFriendlyName = player.getName();
+                if (player.hasPermission(args[1])) {
+                    sender.sendMessage(ChatColor.GREEN + "" + playerFriendlyName + " has permission " + args[1]);
+                    return true;
+                } else {
+                    sender.sendMessage(ChatColor.RED + "" + playerFriendlyName + " does not have permission " + args[1]); }
+                    return true;
+            }
+        }
+    return false; }
 }
